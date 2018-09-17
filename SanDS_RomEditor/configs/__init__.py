a9654ab2dbe9ad02642c05cddb49d61d6d402032
HamanoTaiki <Hamano0813@gmail.com>
2018-09-17 11:36:10 +0800

    #17.重构部分模块名

--- SanDS_RomEditor/configs/__init__.py
+++ SanDS_RomEditor/configs/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from configs.code_mapping import CODE_ALIASES, DECODE_MAPPING, ENCODE_MAPPING, EXPAND_CHARACTER
+from configs.aliases import CODE_ALIASES, DECODE_MAPPING, ENCODE_MAPPING, EXPAND_CHARACTER
