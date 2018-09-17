c636f08956ba0ea4a1e2952d8cd3fc08123ea192
HamanoTaiki <Hamano0813@gmail.com>
2018-09-17 11:04:23 +0800

    #5.新建文本解析设置文件

--- editors/attributes/__init__.py
+++ editors/attributes/__init__.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from attributes.address import Address
+from attributes.length import Length
+from attributes.quantity import Quantity
--- editors/configs/__init__.py
+++ editors/configs/__init__.py
@@ -1,2 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from configs.code_mapping import CODE_ALIASES, DECODE_MAPPING, ENCODE_MAPPING, EXPAND_CHARACTER
