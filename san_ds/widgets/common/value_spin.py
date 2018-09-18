d29019906f32ff3cdc538c0d203f1e226ded6927
HamanoTaiki <Hamano0813@gmail.com>
2018-09-18 13:26:04 +0800

    #50.更新数值微调控件的最大值限制设定

--- san_ds/widgets/common/value_spin.py
+++ san_ds/widgets/common/value_spin.py
@@ -11,20 +11,23 @@ from widgets.abstract import SingleObject
 class ValueSpin(QSpinBox, SingleObject):
     parser_type = Numerical
 
-    def __init__(self, parent, data_name, mapping_name=None, attach=None):
+    def __init__(self, parent, data_name, mapping_name=None, attach=0):
         QSpinBox.__init__(self, parent)
         self.data_name = data_name
         self.mapping_name = mapping_name
-        self.attach = attach
+        self.max_value = attach
         self.data_type: Numerical = self.parser(self.parser_type, data_name)
         if self.data_type.bit is None:
-            self.lineEdit().setValidator(QIntValidator(0, 0x100 ** self.data_type.length.normal_length - 1))
-            self.setRange(0, 0x100 ** self.data_type.length.normal_length - 1)
-            self.max_value = 0x100 ** self.data_type.length.normal_length - 1
+            self.bit_value = 0x100 ** self.data_type.length.normal_length - 1
         else:
-            self.lineEdit().setValidator(QIntValidator(0, 0x2 ** (self.data_type.bit[1] - self.data_type.bit[0]) - 1))
-            self.setRange(0, 0x2 ** (self.data_type.bit[1] - self.data_type.bit[0]) - 1)
-            self.max_value = 0x2 ** (self.data_type.bit[1] - self.data_type.bit[0]) - 1
+            self.bit_value = 0x2 ** (self.data_type.bit[1] - self.data_type.bit[0]) - 1
+        if self.max_value:
+            self.lineEdit().setValidator(QIntValidator(0, self.max_value + 1))
+            self.setRange(0, self.max_value + 1)
+        else:
+            self.max_value = self.bit_value
+            self.lineEdit().setValidator(QIntValidator(0, self.bit_value))
+            self.setRange(0, self.bit_value)
         self.setAlignment(Qt.AlignRight)
 
     def refresh_data(self):
@@ -34,20 +37,21 @@ class ValueSpin(QSpinBox, SingleObject):
         self.data_type.set_data(self.data_index, self.value())
 
     def get_value(self):
+        if self.value() > self.max_value:
+            return self.bit_value
         return self.value()
 
     def set_value(self, value: int):
+        if value > self.max_value:
+            self.setValue(self.max_value + 1)
         self.setValue(value)
-
-    def set_max_value(self, value):
-        self.max_value = value
 
     def textFromValue(self, value):
         if value > self.max_value:
-            return '-'
+            return 'ー'
         return str(value)
 
     def valueFromText(self, text):
-        if text == '-':
+        if text == 'ー':
             return self.max_value
         return int(text)
