c3e0ed6ca44c65e1675fd527b02732cc433da78c
HamanoTaiki <Hamano0813@gmail.com>
2018-09-18 09:27:32 +0800

    #40.更新编辑器背景控件

---
+++ san_ds/widgets/common/background_frame.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QFrame
+from PyQt5.QtCore import Qt
+
+
+class BackgroundFrame(QFrame):
+    def __init__(self, buffer: bytearray):
+        QFrame.__init__(self, parent=None, flags=Qt.FramelessWindowHint)
+        self.buffer = buffer
