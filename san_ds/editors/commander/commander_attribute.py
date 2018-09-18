93bae4c2bfc9904773ec9b27261cae794cdc2780
HamanoTaiki <Hamano0813@gmail.com>
2018-09-18 13:27:30 +0800

    #52.更新武将属性子窗口属性最大值

--- san_ds/editors/commander/commander_attribute.py
+++ san_ds/editors/commander/commander_attribute.py
@@ -8,7 +8,7 @@ from widgets.common.fixed_text import FixedText
 from widgets.common.value_spin import ValueSpin
 from widgets.common.mapping_combo import MappingCombo
 
-Family = {
+family = {
     0: '00.曹操',
     1: '01.劉備',
     2: '02.孫堅',
@@ -29,24 +29,25 @@ Family = {
 class CommanderAttribute(BackgroundFrame):
     def __init__(self, buffer):
         BackgroundFrame.__init__(self, buffer)
-        table = GridTable(self, [(FixedText, '武将属性_姓名'),
-                                 (FixedText, '武将属性_读音'),
-                                 (ValueSpin, '武将属性_武力'),
-                                 (ValueSpin, '武将属性_智力'),
-                                 (ValueSpin, '武将属性_政治'),
-                                 (ValueSpin, '武将属性_魅力'),
-                                 (ValueSpin, '武将属性_陆指'),
-                                 (ValueSpin, '武将属性_水指'),
-                                 (ValueSpin, '武将属性_野望'),
-                                 (ValueSpin, '武将属性_幸运'),
-                                 (ValueSpin, '武将属性_冷静'),
-                                 (ValueSpin, '武将属性_勇猛'),
-                                 (MappingCombo, '武将属性_家族', None, Family),
-                                 (ValueSpin, '武将属性_相性'),
-                                 (ValueSpin, '武将属性_义理'),
-                                 (ValueSpin, '武将属性_生年'),
-                                 (ValueSpin, '武将属性_寿命'),
-                                 ])
+        attribute_table = GridTable(self, [
+            (FixedText, '武将属性_姓名'),
+            (FixedText, '武将属性_读音'),
+            (ValueSpin, '武将属性_武力', None, 100),
+            (ValueSpin, '武将属性_智力', None, 100),
+            (ValueSpin, '武将属性_政治', None, 100),
+            (ValueSpin, '武将属性_魅力', None, 100),
+            (ValueSpin, '武将属性_陆指', None, 100),
+            (ValueSpin, '武将属性_水指', None, 100),
+            (ValueSpin, '武将属性_野望'),
+            (ValueSpin, '武将属性_幸运'),
+            (ValueSpin, '武将属性_冷静'),
+            (ValueSpin, '武将属性_勇猛'),
+            (ValueSpin, '武将属性_相性'),
+            (ValueSpin, '武将属性_义理'),
+            (ValueSpin, '武将属性_生年'),
+            (ValueSpin, '武将属性_寿命'),
+            (MappingCombo, '武将属性_家族', None, family),
+        ])
         layout = QGridLayout()
-        layout.addWidget(table)
+        layout.addWidget(attribute_table)
         self.setLayout(layout)
