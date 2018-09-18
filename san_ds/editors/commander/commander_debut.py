7e003554fbe0fc42e896502c17611b3ca147433c
HamanoTaiki <Hamano0813@gmail.com>
2018-09-18 13:27:03 +0800

    #51.更新武将登场设定子窗口

---
+++ san_ds/editors/commander/commander_debut.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QGridLayout
+from widgets.common.background_frame import BackgroundFrame
+from widgets.common.grid_table import GridTable, GridModel
+from widgets.common.fixed_text import FixedText
+from widgets.common.value_spin import ValueSpin
+from widgets.common.mapping_combo import MappingCombo
+from widgets.common.control_combo import ControlCombo
+from attributes import Quantity
+
+identity = {
+    0: '君主[流浪]',
+    1: '军师',
+    2: '将军',
+    3: '武将',
+    4: '文官',
+    5: '流浪跟随',
+    6: '在野',
+    7: '未登场',
+    128: '君主',
+    129: '军师[太守]',
+    130: '将军[太守]',
+    131: '武将[太守]',
+    132: '文官[太守]',
+    255: '死亡'
+}
+
+debut_offsets = [0x0,
+                 -0xC04,
+                 -0x1808,
+                 -0x240C,
+                 -0x3010,
+                 -0x3C14,
+                 0xC04]
+
+
+class CommanderDebut(BackgroundFrame):
+    def __init__(self, buffer):
+        BackgroundFrame.__init__(self, buffer)
+
+        scenario_combo = ControlCombo(self, '剧本属性_剧本名')
+
+        scenario_debut = GridTable(self, [
+            (FixedText, '武将属性_姓名'),
+            (ValueSpin, '武将登场_登场年'),
+            (MappingCombo, '武将登场_血缘', '武将属性_姓名', True),
+            (MappingCombo, '武将登场_都市', '都市属性_名称', True),
+            (MappingCombo, '武将登场_身份', None, identity),
+            (ValueSpin, '武将登场_忠诚', None, 100),
+            (ValueSpin, '武将登场_士兵数', None, 200),
+        ], Quantity(0x301))
+
+        scenario_combo.add_control_target(scenario_debut.model().column_objects[3].data_type.set_offset, debut_offsets)
+        scenario_combo.add_control_target(scenario_debut.model().column_objects[4].data_type.set_offset, debut_offsets)
+        scenario_combo.add_control_target(scenario_debut.model().column_objects[5].data_type.set_offset, debut_offsets)
+        scenario_combo.add_control_target(scenario_debut.model().column_objects[6].data_type.set_offset, debut_offsets)
+
+        layout = QGridLayout()
+        layout.addWidget(scenario_combo, 0, 0, 1, 2)
+        layout.addWidget(scenario_debut, 1, 0, 1, 1)
+        self.setLayout(layout)
