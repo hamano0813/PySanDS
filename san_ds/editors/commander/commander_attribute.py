0a79013b0951f8fbb34486afa04f2bd70d842c02
HamanoTaiki <Hamano0813@gmail.com>
2018-09-18 10:24:19 +0800

    #43.更新子窗口

---
+++ san_ds/editors/commander/commander_attribute.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QGridLayout
+from widgets.common.background_frame import BackgroundFrame
+from widgets.common.grid_table import GridTable
+from widgets.common.fixed_text import FixedText
+from widgets.common.value_spin import ValueSpin
+from widgets.common.mapping_combo import MappingCombo
+
+Family = {
+    0: '00.曹操',
+    1: '01.劉備',
+    2: '02.孫堅',
+    3: '03.袁紹',
+    4: '04.袁術',
+    5: '05.馬騰',
+    6: '06.劉焉',
+    7: '07.劉表',
+    8: '08.董卓',
+    9: '09.公孫瓚',
+    10: '0A.張魯',
+    11: '0B.孟獲',
+    12: '0C.荀彧',
+    255: 'FF.ーー'
+}
+
+
+class CommanderAttribute(BackgroundFrame):
+    def __init__(self, buffer):
+        BackgroundFrame.__init__(self, buffer)
+        table = GridTable(self, [(FixedText, '武将属性_姓名'),
+                                 (FixedText, '武将属性_读音'),
+                                 (ValueSpin, '武将属性_武力'),
+                                 (ValueSpin, '武将属性_智力'),
+                                 (ValueSpin, '武将属性_政治'),
+                                 (ValueSpin, '武将属性_魅力'),
+                                 (ValueSpin, '武将属性_陆指'),
+                                 (ValueSpin, '武将属性_水指'),
+                                 (ValueSpin, '武将属性_野望'),
+                                 (ValueSpin, '武将属性_幸运'),
+                                 (ValueSpin, '武将属性_冷静'),
+                                 (ValueSpin, '武将属性_勇猛'),
+                                 (MappingCombo, '武将属性_家族', None, Family),
+                                 (ValueSpin, '武将属性_相性'),
+                                 (ValueSpin, '武将属性_义理'),
+                                 (ValueSpin, '武将属性_生年'),
+                                 (ValueSpin, '武将属性_寿命'),
+                                 ])
+        layout = QGridLayout()
+        layout.addWidget(table)
+        self.setLayout(layout)
