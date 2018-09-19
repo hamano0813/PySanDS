554595d8be44faa4901e9cf2a9b477d45cd9c44b
HamanoTaiki <Hamano0813@gmail.com>
2018-09-19 12:37:02 +0800

    #64.更新多行文本框

---
+++ san_ds/widgets/common/multiline_text.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QTextEdit
+from PyQt5.QtCore import QRegExp
+from parsers import Character
+from widgets.abstract import SingleObject
+from configs import EXPAND_CHARACTER
+
+
+class MultilineText(QTextEdit, SingleObject):
+    parser_type = Character
+
+    def __init__(self, parent, data_name, mapping_name=None, attach=None):
+        QTextEdit.__init__(self, parent)
+        self.data_name = data_name
+        self.mapping_name = mapping_name
+        self.attach = attach
+        self.data_type: Character = self.parser(self.parser_type, data_name)
+        char_file = open('./configs/character.txt', 'r', encoding='UTF-8')
+        regex_char = char_file.read() + EXPAND_CHARACTER
+        char_file.close()
+        self.reg_exp = QRegExp(f'[{regex_char}\\n\\r]+')
+
+    def refresh_data(self):
+        self.setText(self.data_type.get_data(self.data_index))
+
+    def save_data(self):
+        self.data_type.set_data(self.data_index, self.toPlainText())
+        self.refresh_data()
+
+    def get_value(self):
+        return self.toPlainText()
+
+    def set_value(self, text):
+        self.setText(text)
